7e76b244bc9734baf05a4faf0cb66691924a3cc0
Aizen <845283394@qq.com>
2018-09-01 18:19:42 +0800

    add AZRouter

---
+++ AZFunction/AZRouter.swift
@@ -0,0 +1,41 @@
+//
+//  AZRouter.swift
+//  AZFunction
+//
+//  Created by 秦叶 on 2018/9/1.
+//  Copyright © 2018年 Aizen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class AZRouter {
+    
+    @discardableResult
+    public static func perform(spaceName:String, targetName: String, actionName: String, parameters: [String: Any]? = nil) -> Any? {
+        
+        guard let target: AnyObject  = NSClassFromString(spaceName+"."+targetName) else {return nil}
+        
+        if actionName.isEmpty{//无方法时操作
+            
+            guard let clsType = target as? NSObject.Type else {return nil}
+            let obj = clsType.init()
+            return obj
+            
+        }else{
+            
+            let action = NSSelectorFromString(actionName)
+            if target.responds(to: action) {//调用方法为@objc类方法
+                if parameters == nil {
+                    return target.perform(action).takeUnretainedValue()
+                } else {
+                    return target.perform(action, with:parameters!).takeUnretainedValue()
+                }
+            }  else {
+                return nil
+            }
+            
+        }
+    }
+    
+}
---
+++ AZFunctionTests/AZRouterTests.swift
@@ -0,0 +1,16 @@
+//
+//  AZRouterTests.swift
+//  AZFunctionTests
+//
+//  Created by 秦叶 on 2018/9/1.
+//  Copyright © 2018年 Aizen. All rights reserved.
+//
+
+import Foundation
+
+class AZRouterTests: NSObject {
+    func test() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        print("test")
+    }
+}
