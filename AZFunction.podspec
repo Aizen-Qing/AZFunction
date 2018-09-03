5f85474676bbc7b5f974d6bac3a3fc5aaa781536
Aizen <845283394@qq.com>
2018-09-03 21:31:19 +0800

    refresh

--- AZFunction.podspec
+++ AZFunction.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "AZFunction"
-  s.version      = "1.0.6"
+  s.version      = "1.0.7"
   s.summary      = "AZFunction is help function"
   s.description  = <<-DESC
 			AZFunction is aizen's help function.
--- AZFunction/AZDebug.swift
+++ AZFunction/AZDebug.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-func print_debug<T>(message:T, fileName:String = #file, methodName:String = #function, lineNumber:Int = #line){
+public func print_debug<T>(message:T, fileName:String = #file, methodName:String = #function, lineNumber:Int = #line){
     #if DEBUG
     let fileStr : String = (fileName as NSString).pathComponents.last!.replacingOccurrences(of: "swift", with: "")
     print("\(fileStr)\(methodName)[\(lineNumber)]:\(message)")
